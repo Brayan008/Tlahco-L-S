8da61569f3b7634a28ac3222df2c238483f1c92e
Brayan <mares.com>
2020-10-30 13:20:43 -0600

    Agregacion api de Stream

--- mux/twitch.js
+++ mux/twitch.js
@@ -1,6 +1,6 @@
 //Create a Twitch.Embed object that will render within the "twitch-embed" root element.
   new Twitch.Embed("stream-mux", {
-    width: 600,
-    height: 280,
+    width: 854,
+    height: 480,
     channel: "bmb008"
   });
